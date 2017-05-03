d0b8b8940cec386651ec2c5765d88b2d578dd4dc
Oleg Babiy <olegbabiy.ob@gmail.com>
2017-05-03 17:58:24 +0300

    Add mocha chai

--- test/book_reducer_spec.js
+++ test/book_reducer_spec.js
@@ -1,15 +1,15 @@
-// import {Map, fromJS} from 'immutable';
-// import {expect} from 'chai';
-// import reducer from '../src/reducers/book_reducer';
+import {Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+// import reducer from '../src/reducers/books_reducer';
 
-// describe('book_reducer', ()=>{
-// 	 it('handles GET_BOOKS', () => {
-//     const initialState = Map();
-//     const action = {type: 'GET_BOOKS', all: []};
-//     const nextState = reducer(initialState, action);
+describe('book_reducer', ()=>{
+	 it('handles GET_BOOKS', () => {
+  //   const initialState = Map();
+  //   const action = {type: 'GET_BOOKS', all: []};
+  //   const nextState = reducer(initialState, action);
 
-//     expect(nextState).to.equal(fromJS({
-//       all: []
-//     }));
-//   });
-// });
+    // expect(nextState).to.equal(fromJS({
+    //   all: []
+    // }));
+  });
+});
